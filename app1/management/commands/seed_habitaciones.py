46866a4a5103d32d545adb03bc87167b0cd15349
taoista <taoista-@hotmail.com>
2024-07-12 17:26:42 -0400

    sin terminar proyecto

---
+++ app1/management/commands/seed_habitaciones.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from app1.models import Habitaciones
+
+class Command(BaseCommand):
+    help = 'Crea 5 registros de habitaciones'
+
+    def handle(self, *args, **kwargs):
+        # Crear una lista de datos para los hoteles
+        habitaciones_data = [
+            {'titulo': 'habitacion 1', 'caracteristicas':'carac 1', 'precio':5000},
+            {'titulo': 'habitacion 2', 'caracteristicas':'carac 2', 'precio':7000},
+            {'titulo': 'habitacion 3', 'caracteristicas':'carac 3, carac 2', 'precio':8500},
+           
+        ]
+        
+        # Iterar sobre los datos y crear registros en la base de datos
+        for data in habitaciones_data:
+            Habitaciones.objects.create(**data)
+        
+        self.stdout.write(self.style.SUCCESS('5 registros creados'))
+
---
+++ app1/management/commands/seed_serv_extras.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from app1.models import ServiciosExtras
+
+class Command(BaseCommand):
+    help = 'Crea 5 registros de servicios extras'
+
+    def handle(self, *args, **kwargs):
+        # Crear una lista de datos para los hoteles
+        servicios_datos = [
+            {'titulo': 'servicio 1','precio':5000},
+            {'titulo': 'servicio 2','precio':7000},
+            {'titulo': 'servicio 3','precio':8000},
+           
+        ]
+        
+        # Iterar sobre los datos y crear registros en la base de datos
+        for data in servicios_datos:
+            ServiciosExtras.objects.create(**data)
+        
+        self.stdout.write(self.style.SUCCESS('5 registros creados servicios extras'))
+
